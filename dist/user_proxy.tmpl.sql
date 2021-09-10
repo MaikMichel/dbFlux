9324acff378e42b991e2d816ed7e99bd1398dd7a
Maik Michel <maik.michel78@gmail.com>
2021-09-10 13:02:52 +0200

    Add script templates for user generation

---
+++ dist/user_proxy.tmpl.sql
@@ -0,0 +1,55 @@
+set define '^'
+set verify off
+
+prompt
+prompt
+prompt **********************************************************************
+prompt ***  USER CREATION: {{proxy_user}}
+prompt **********************************************************************
+prompt
+prompt
+
+prompt {{proxy_user}} droppen
+declare
+  v_check number(1) := 0;
+begin
+  select 1
+    into v_check
+    from all_users
+   where username = upper('{{proxy_user}}');
+  dbms_output.put_line('drop user {{proxy_user}} cascade');
+  execute immediate 'drop user {{proxy_user}} cascade';
+exception
+  when no_data_found then
+    null; -- ok, nothing to drop  ´
+end;
+/
+
+prompt create user {{proxy_user}} identified by {{db_app_pwd}} default tablespace ^deftablespace
+create user {{proxy_user}}
+  identified by {{db_app_pwd}}
+  default tablespace users
+  temporary tablespace temp
+  profile default
+  account unlock;
+
+
+
+-- 2 roles for {{proxy_user}}
+grant connect to {{proxy_user}};
+alter user {{proxy_user}} default role all;
+grant create any context to {{proxy_user}};
+
+alter user {{data_schema}}
+  grant connect through {{proxy_user}};
+
+alter user {{logic_schema}}
+  grant connect through {{proxy_user}};
+
+alter user {{app_schema}}
+  grant connect through {{proxy_user}};
+
+
+prompt **********************************************************************
+prompt
+prompt
---
+++ dist/user_single_app.tmpl.sql
@@ -0,0 +1,69 @@
+set define '^'
+set verify off
+
+
+prompt
+prompt
+prompt **********************************************************************
+prompt ***  SCHEMA CREATION: {{data_schema}}
+prompt **********************************************************************
+prompt
+prompt
+
+
+prompt {{data_schema}} droppen
+declare
+  v_check number(1) := 0;
+begin
+  select 1
+    into v_check
+    from all_users
+   where username = upper('{{data_schema}}');
+  dbms_output.put_line('drop user {{data_schema}} cascade');
+  execute immediate 'drop user {{data_schema}} cascade';
+exception
+  when no_data_found then
+    null; -- ok, nothing to drop  ´
+end;
+/
+
+prompt create user {{data_schema}} default tablespace users
+create user {{data_schema}} identified by "{{db_app_pwd}}"
+  default tablespace users
+  temporary tablespace temp
+  profile default
+  account unlock;
+
+
+-- 2 tablespace quotas for {{data_schema}}
+alter user {{data_schema}} quota unlimited on users;
+
+-- 2 roles for {{data_schema}}
+alter user {{data_schema}} default role all;
+
+-- 11 system privileges for {{data_schema}}
+grant create any context to {{data_schema}};
+grant create any directory to {{data_schema}};
+grant create any procedure to {{data_schema}};
+grant create job to {{data_schema}};
+grant create procedure to {{data_schema}};
+grant create sequence to {{data_schema}};
+grant create synonym to {{data_schema}};
+grant create public synonym to {{data_schema}};
+grant create table to {{data_schema}};
+grant create trigger to {{data_schema}};
+grant create type to {{data_schema}};
+grant create view to {{data_schema}};
+grant create session to {{data_schema}};
+
+-- 5 object privileges for {{data_schema}}
+grant execute on sys.dbms_crypto to {{data_schema}};
+grant execute on sys.utl_file to {{data_schema}};
+grant execute on sys.utl_http to {{data_schema}};
+grant execute on sys.dbms_rls to {{data_schema}};
+
+grant create any context to {{data_schema}};
+
+prompt **********************************************************************
+prompt
+prompt
