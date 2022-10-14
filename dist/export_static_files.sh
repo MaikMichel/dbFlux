2e7acb47dd092b268e3f633f5c25f978e800c7eb
Maik Michel <maik.michel78@gmail.com>
2022-10-14 16:48:00 +0200

    Add workaround to output base64 clob data when user is using SQLcl #6

--- dist/export_static_files.sh
+++ dist/export_static_files.sh
@@ -38,19 +38,20 @@ echo -e "${BYELLOW}Target:${NC}        ${WHITE}${DBFLOW_EXP_PATH}${NC}"
 echo -e "${CYAN}$(date '+%d.%m.%Y %H:%M:%S') >> exporting Files from ${DBFLOW_EXP_APP_ID} to static/${DBFLOW_EXP_APP_ID}/src ${NC}"
 echo -e "${CYAN}$(date '+%d.%m.%Y %H:%M:%S') >> ... this may take a while ${NC}"
 
-## FullExport
-PREPSTMT=":contents := to_base64(get_zip(p_app_id => ${DBFLOW_EXP_APP_ID}));"
-
-## Or just an object
-if [[ -n ${DBFLOW_EXP_FNAME} ]]; then
-  PREPSTMT=":contents :=  to_base64(get_zip(p_app_id => ${DBFLOW_EXP_APP_ID}, p_file_name => '${DBFLOW_EXP_FNAME}'));"
-fi
 
 ANOFUNCTIONS=$( cat "${SCRIPT_DIR}/export_app_static_function.sql" )
 
-
 # the export itself
-${DBFLOW_SQLCLI} -s -l "${DBFLOW_DBUSER}/${DBFLOW_DBPASS}@${DBFLOW_DBTNS}" <<! > "${DBFLOW_EXP_PATH}".zip.base64
+if [[ ${DBFLOW_SQLCLI} == "sqlplus" ]]; then
+  ## FullExport
+  PREPSTMT=":contents := to_base64(get_zip(p_app_id => ${DBFLOW_EXP_APP_ID}));"
+
+  ## Or just an object
+  if [[ -n ${DBFLOW_EXP_FNAME} ]]; then
+    PREPSTMT=":contents :=  to_base64(get_zip(p_app_id => ${DBFLOW_EXP_APP_ID}, p_file_name => '${DBFLOW_EXP_FNAME}'));"
+  fi
+
+  sqlplus -s -l "${DBFLOW_DBUSER}/${DBFLOW_DBPASS}@${DBFLOW_DBTNS}" <<! > "${DBFLOW_EXP_PATH}".zip.base64
     set verify off
     set scan off
     set feedback off
@@ -74,28 +75,67 @@ ${DBFLOW_SQLCLI} -s -l "${DBFLOW_DBUSER}/${DBFLOW_DBPASS}@${DBFLOW_DBTNS}" <<! >
 
 !
 
-if [[ -f "${DBFLOW_EXP_PATH}.zip.base64" ]]; then
-  #if grep -q "ORA-20001:" "${DBFLOW_EXP_PATH}.zip.base64"; then
-  if grep -q "ORA-.*:" "${DBFLOW_EXP_PATH}.zip.base64"; then
-    echo -e "${RED}Error detected on export${NC}"
-    tput setaf 9
-    cat "${DBFLOW_EXP_PATH}.zip.base64"
-    tput setaf default
+else
+  ## FullExport
+  PREPSTMT="v_content := to_base64(get_zip(p_app_id => ${DBFLOW_EXP_APP_ID}));"
 
-    rm "${DBFLOW_EXP_PATH}.zip.base64"
-  else
-    echo -e "${CYAN}$(date '+%d.%m.%Y %H:%M:%S') >> Decoding exported static file ... ${NC}"
-    base64 -di "${DBFLOW_EXP_PATH}.zip.base64" > "${DBFLOW_EXP_PATH}.zip"
-
-    # remove base64 garbage
-    rm "${DBFLOW_EXP_PATH}.zip.base64"
+  ## Or just an object
+  if [[ -n ${DBFLOW_EXP_FNAME} ]]; then
+    PREPSTMT="v_content := to_base64(get_zip(p_app_id => ${DBFLOW_EXP_APP_ID}, p_file_name => '${DBFLOW_EXP_FNAME}'));"
   fi
 
-  # unzip file content
-  if [[ -f "${DBFLOW_EXP_PATH}.zip" ]]; then
-    echo -e "${CYAN}$(date '+%d.%m.%Y %H:%M:%S') >> Unzipping exported schema file ... ${NC}"
-    unzip -o "${DBFLOW_EXP_PATH}.zip" -d "${DBFLOW_EXP_PATH}"
-    rm "${DBFLOW_EXP_PATH}.zip"
-    echo -e "${GREEN}$(date '+%d.%m.%Y %H:%M:%S') >> export done${NC}"
-  fi
+  sql -s -l "${DBFLOW_DBUSER}/${DBFLOW_DBPASS}@${DBFLOW_DBTNS}" <<! > "${DBFLOW_EXP_PATH}".zip.base64
+    set verify off
+    set scan off
+    set feedback off
+    set heading off
+    set trimout on
+    set trimspool on
+    set pagesize 0
+    set linesize 5000
+    set long 100000000
+    set longchunksize 32767
+    set serveroutput on
+    whenever sqlerror exit sql.sqlcode rollback
+    rem variable contents clob
+    DECLARE
+      v_content clob;
+      ${ANOFUNCTIONS}
+    BEGIN
+      ${PREPSTMT}
+      print_clob_to_output(v_content);
+    END;
+    /
+
+!
+
 fi
+
+
+ if [[ -f "${DBFLOW_EXP_PATH}.zip.base64" ]]; then
+   #if grep -q "ORA-20001:" "${DBFLOW_EXP_PATH}.zip.base64"; then
+   if grep -q "ORA-.*:" "${DBFLOW_EXP_PATH}.zip.base64"; then
+     echo -e "${RED}Error detected on export${NC}"
+     tput setaf 9
+     cat "${DBFLOW_EXP_PATH}.zip.base64"
+     tput setaf default
+
+     rm "${DBFLOW_EXP_PATH}.zip.base64"
+   else
+     echo -e "${CYAN}$(date '+%d.%m.%Y %H:%M:%S') >> Decoding exported static file ... ${NC}"
+     base64 -di "${DBFLOW_EXP_PATH}.zip.base64" > "${DBFLOW_EXP_PATH}.zip"
+
+     # remove base64 garbage
+     rm "${DBFLOW_EXP_PATH}.zip.base64"
+   fi
+
+   # unzip file content
+   if [[ -f "${DBFLOW_EXP_PATH}.zip" ]]; then
+     echo -e "${CYAN}$(date '+%d.%m.%Y %H:%M:%S') >> Unzipping exported schema file ... ${NC}"
+     unzip -o "${DBFLOW_EXP_PATH}.zip" -d "${DBFLOW_EXP_PATH}"
+     rm "${DBFLOW_EXP_PATH}.zip"
+     echo -e "${GREEN}$(date '+%d.%m.%Y %H:%M:%S') >> export done${NC}"
+   fi
+ fi
+
+#############################################################################################################################
